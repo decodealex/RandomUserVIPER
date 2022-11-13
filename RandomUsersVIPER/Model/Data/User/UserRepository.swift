f48e20cc68ae425350581d91046a83cb9637fe30
AlexKov <kovalyshyn.alex@gmail.com>
2022-11-12 20:04:07 -0800

    Implemented User Data Layer
    - Implemented UserRepository
    - Implemented UserRemoteDataSource

--- RandomUsersVIPER/Model/Data/User/UserRepository.swift
+++ RandomUsersVIPER/Model/Data/User/UserRepository.swift
@@ -1,8 +1,28 @@
 //
-//  UserRepositoryProtocol.swift
+//  UserRepository.swift
 //  RandomUsersVIPER
 //
 //  Created by Alex Kovalyshyn on 12.11.2022.
 //
 
 import Foundation
+
+protocol UserRepositoryProtocol {
+    mutating func getUserList(of count: Int) async throws -> [User]
+}
+
+struct UserRepository {
+    private let remoteDataSource: UserRemoteDataSourceProtocol
+    private var users: [User] = []
+    
+    init(_ remoteDataSource: UserRemoteDataSourceProtocol? = nil) {
+        self.remoteDataSource = remoteDataSource ?? UserRemoteDataSource()
+    }
+}
+
+extension UserRepository: UserRepositoryProtocol {
+    mutating func getUserList(of count: Int) async throws -> [User] {
+        users = try await remoteDataSource.getUserList(of: count)
+        return users
+    }
+}
