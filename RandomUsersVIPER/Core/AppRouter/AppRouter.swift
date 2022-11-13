c8bdc9e037f279d43e5c8863bef087e034cec81b
AlexKov <kovalyshyn.alex@gmail.com>
2022-11-12 20:03:12 -0800

    Implemented AppRouter
    - Implemented AppRouter

--- RandomUsersVIPER/Core/AppRouter/AppRouter.swift
+++ RandomUsersVIPER/Core/AppRouter/AppRouter.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import UIKit
+
+protocol AppRouterProtocol {
+    func start()
+}
+
+class AppRouter: AppRouterProtocol {
+    private let window: UIWindow?
+    
+    init(window: UIWindow?) {
+        self.window = window
+    }
+    
+    func start() {
+        let rootViewController = UserListRouter.buildModule()
+        let rootNavigationController = UINavigationController(rootViewController: rootViewController)
+        
+        window?.rootViewController = rootNavigationController
+        window?.makeKeyAndVisible()
+    }
+}
